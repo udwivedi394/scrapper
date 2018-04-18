11f09e32bc12a2af833b8a0205f0b0c5e5065b05
udwivedi394 <utkarshdwivedi394@gmail.com>
2018-04-18 09:56:55 +0530

    The current structure of code is inadequate for paging, do the necessary modifications

--- fb_post.py
+++ fb_post.py
@@ -22,19 +22,20 @@ def getCurlData02(url):
             print("Retrying...")
     return response.json()
 
-def getFBData(page_id,access_token,edge):
+def getFBData(page_id,access_token,edge,lim_num):
     url1 = "https://graph.facebook.com/v2.11/"
     url2 = "%s/%s"%(page_id,edge)
-    url3 = "&access_token=%s"%(access_token)
+    url4 = "&limit=%d"%(lim_num)
+    url5 = "&access_token=%s"%(access_token)
     
     #adding datelimit
     tt = datetime.timedelta(6*365/12)
     dd = datetime.datetime.now()
     back_date = (dd-tt).strftime("%Y-%m-%d")
     cur_date = dd.strftime("%Y-%m-%d")
-    url4 = "/?since=%s&until=%s"%(back_date,cur_date)
+    url3 = "/?since=%s&until=%s"%(back_date,cur_date)
     
-    url = url1+url2+url4+url3
+    url = url1+url2+url3+url4+url5
     
     data = getCurlData02(url)
     return data.get('data')
@@ -45,20 +46,28 @@ if __name__=="__main__":
     with open('%s_fb_post_data.csv' % page_id, 'w', newline='') as csvfile:
         csvf = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csvf.writerow(["id","parent_id","created_time","message"])
- 
-        posts = getFBData(page_id,access_token,'posts')
-        for feed in posts:
+
+        next_page_available = True
+
+        while next_page_available: 
+            posts = getFBData(page_id,access_token,'posts',100)
+            for feed in posts:
             
-            if feed.get('message')==None:
-                continue
+                if feed.get('message')==None:
+                    continue
             
-            csvf.writerow([feed['id'],'NULL',feed['created_time'],feed['message']])
-            commentdata = getFBData(feed['id'],access_token,'comments')
+                csvf.writerow([feed['id'],'NULL',feed['created_time'],feed['message']])
+                commentdata = getFBData(feed['id'],access_token,'comments',100)
             
-            if commentdata==None:
-                continue
+                if commentdata==None:
+                    continue
+                
+                for feed2 in commentdata:
+                    csvf.writerow([feed2['id'],feed['id'],feed2['created_time'],feed2['message']])
             
-            for feed2 in commentdata:
-                csvf.writerow([feed2['id'],feed['id'],feed2['created_time'],feed2['message']])
+            if 'paging' in posts.keys():
+                posts = json.loads(getCurlData02(statuses['paging']['next']))
+            else:
+                next_page_available=False
 
         print("Done")
